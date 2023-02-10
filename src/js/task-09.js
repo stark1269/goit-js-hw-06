79bf4bbfbcf131a0d464a74cc84a40cb02d78d21
stark1269 <stark1269@gmail.com>
2023-02-10 23:01:24 +0200

    add 9 task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -2,4 +2,15 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const colorBtn = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+colorBtn.addEventListener('click', randomBodyColor);
+
+function randomBodyColor() {
+  const bodyBg = document.querySelector('body');
+  bodyBg.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = bodyBg.style.backgroundColor;
+};
+
 
