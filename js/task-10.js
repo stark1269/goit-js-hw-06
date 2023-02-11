842f26dd17ddc239ad705e4eebcb9422be2997c9
stark1269 <stark1269@gmail.com>
2023-02-11 10:29:36 +0200

    add 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,38 @@
+const refs = {
+  input: document.querySelector('#controls').firstElementChild,
+  create: document.querySelector('button[data-create]'),
+  destroy: document.querySelector('button[data-destroy]'),
+  box: document.querySelector('#boxes'),
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+function createBoxes(amount) {
+  const divArray = [];
 
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement('div');
+
+    div.style.height = `${30 + 10 * i}px`;
+    div.style.width = `${30 + 10 * i}px`;
+    div.style.backgroundColor = getRandomHexColor();
+
+    divArray.push(div);
+  }
+  return divArray
+};
+
+function addElementsInBox() {
+  const ElementsBox = createBoxes(refs.input.value);
+  refs.box.append(...ElementsBox);
+}
+
+function destroyBoxes() {
+  refs.box.innerHTML = '';
+};
+
+refs.create.addEventListener('click', addElementsInBox);
+
+refs.destroy.addEventListener('click', destroyBoxes);
